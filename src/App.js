69ed20fc1a233d4007b956491890fdd3baf47e67
Nikhil <nikhilbagri18@gmail.com>
2023-03-19 03:27:49 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './Calculator.css';
 
-function App() {
+function Calculator() {
+  const [displayValue, setDisplayValue] = useState('0');
+  const [firstOperand, setFirstOperand] = useState(null);
+  const [waitingForSecondOperand, setWaitingForSecondOperand] = useState(false);
+  const [operator, setOperator] = useState(null);
+
+  function inputDigit(digit) {
+    if (waitingForSecondOperand) {
+      setDisplayValue(String(digit));
+      setWaitingForSecondOperand(false);
+    } else {
+      setDisplayValue(displayValue === '0' ? String(digit) : displayValue + digit);
+    }
+  }
+
+  function inputDecimal() {
+    if (waitingForSecondOperand) return;
+    if (!displayValue.includes('.')) {
+      setDisplayValue(displayValue + '.');
+    }
+  }
+
+  function clearDisplay() {
+    setDisplayValue('0');
+    setFirstOperand(null);
+    setWaitingForSecondOperand(false);
+    setOperator(null);
+  }
+
+  function performOperation(nextOperator) {
+    const inputValue = parseFloat(displayValue);
+
+    if (firstOperand === null) {
+      setFirstOperand(inputValue);
+    } else if (operator) {
+      const result = calculate(firstOperand, inputValue, operator);
+
+      setDisplayValue(String(result));
+      setFirstOperand(result);
+    }
+
+    setWaitingForSecondOperand(true);
+    setOperator(nextOperator);
+  }
+
+  function calculate(firstOperand, secondOperand, operator) {
+    if (operator === '+') {
+      return firstOperand + secondOperand;
+    } else if (operator === '-') {
+      return firstOperand - secondOperand;
+    } else if (operator === '*') {
+      return firstOperand * secondOperand;
+    } else if (operator === '/') {
+      return firstOperand / secondOperand;
+    }
+
+    return secondOperand;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator">
+      <div className="display">{displayValue}</div>
+      <div className="keypad">
+        <button className="gray" onClick={() => clearDisplay()}>C</button>
+        <button className="gray">±</button>
+        <button className="gray">%</button>
+        <button className="orange" onClick={() => performOperation('/')}>÷</button>
+        <button onClick={() => inputDigit(7)}>7</button>
+        <button onClick={() => inputDigit(8)}>8</button>
+        <button onClick={() => inputDigit(9)}>9</button>
+        <button className="orange" onClick={() => performOperation('*')}>×</button>
+        <button onClick={() => inputDigit(4)}>4</button>
+        <button onClick={() => inputDigit(5)}>5</button>
+        <button onClick={() => inputDigit(6)}>6</button>
+        <button className="orange" onClick={() => performOperation('-')}>-</button>
+        <button onClick={() => inputDigit(1)}>1</button>
+        <button onClick={() => inputDigit(2)}>2</button>
+        <button onClick={() => inputDigit(3)}>3</button>
+        <button className="orange" onClick={() => performOperation('+')}>+</button>
+        <button className="zero" onClick={() => inputDigit(0)}>0</button>
+        <button onClick={() => inputDecimal()}>.</button>
+        <button className="orange" onClick={() => performOperation('=')}>=</button>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default Calculator;
